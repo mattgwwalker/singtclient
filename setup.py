b3af2a35e3edc214054ad3fcd109158d5cacb1af
Matthew Walker <mattgwwalker@gmail.com>
2020-08-08 15:55:30 +1200

    Added scipy requirement

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='singtclient',
-      version='0.10.0',
+      version='0.10.1',
       packages=find_packages(), #["singtclient"],
       include_package_data=True,
       install_requires=[
           "art",
           "numpy",
           "pyogg",
+          "scipy",
           "singtcommon",
           "sounddevice",
           "twisted",
